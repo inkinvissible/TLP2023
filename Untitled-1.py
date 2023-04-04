e467f3bcc38485d49c25d510a910590182704385
inkinvissible <agusmartin113@gmail.com>
2023-04-04 10:28:16 -0300

    commit message

---
+++ Untitled-1.py
@@ -0,0 +1,37 @@
+# def opcion_1():
+#     print("Has elegido la opción 1")
+
+# def opcion_2():
+#     print("Has elegido la opción 2")
+
+# def opcion_3():
+#     print("Has elegido la opción 3")
+
+# def opcion_4():
+#     print("Has elegido la opción 4")
+
+# def opcion_5():
+#     print("Has elegido la opción 5")
+
+# # Creamos el diccionario con cada opción y su respectiva función
+# opciones = {
+#     1: opcion_1,
+#     2: opcion_2,
+#     3: opcion_3,
+#     4: opcion_4,
+#     5: opcion_5
+# }
+
+# # Pedimos al usuario que elija una opción
+# opcion = int(input("Elige una opción: \n1) Cargar Cliente\n2) Eliminar Cliente\n3) Ver Clientes\n4) Listar clientes\n5) Hola"))
+
+# #
+
+# # Llamamos a la función correspondiente a la opción elegida
+# opciones[opcion]()
+
+
+lado = int(input("Ingrese la medida del lado del cuadrado\n"))
+superficie = lado*lado
+print("La sup del cuadrado es: ")
+print(superficie)
